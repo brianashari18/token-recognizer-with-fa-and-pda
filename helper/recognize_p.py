ce7a8feb306c6607ae07aae5e1918627f79e01fb
Althaf Rizqullah <althaf8747@gmail.com>
2024-06-18 09:43:57 +0700

    changing helper s and p

--- helper/recognize_p.py
+++ helper/recognize_p.py
@@ -2,74 +2,44 @@ class Recognizer_P:
     def __init__(self):
         # Definisi transisi state untuk mesin keadaan terbatas (finite state machine)
         self.transitions = {
-            "bermain": {
-                0: {"b": 1},
-                1: {"e": 2},
-                2: {"r": 3},
-                3: {"m": 4},
-                4: {"a": 5},
-                5: {"i": 6},
-                6: {"n": 100},
-            },
-            "membaca": {
-                0: {"m": 1},
-                1: {"e": 2},
-                2: {"m": 3},
-                3: {"b": 4},
-                4: {"a": 5},
-                5: {"c": 6},
-                6: {"a": 100},
-            },
-            "belajar": {
-                0: {"b": 1},
-                1: {"e": 2},
-                2: {"l": 3},
-                3: {"a": 4},
-                4: {"j": 5},
-                5: {"a": 6},
-                6: {"r": 100},
-            },
-            "menonton": {
-                0: {"m": 1},
-                1: {"e": 2},
-                2: {"n": 3},
-                3: {"o": 4},
-                4: {"n": 5},
-                5: {"t": 6},
-                6: {"o": 7},
-                7: {"n": 100},
-            },
-            "membeli": {
-                0: {"m": 1},
-                1: {"e": 2},
-                2: {"m": 3},
-                3: {"b": 4},
-                4: {"e": 5},
-                5: {"l": 6},
-                6: {"i": 100},
-            },
+            0: {'b': 1, 'm': 7},
+            1: {'e': 2},
+            2: {'r': 3, 'l': 9},
+            3: {'m': 4},
+            4: {'a': 5},
+            5: {'i': 6},
+            6: {'n': 100},  # "bermain"
+            7: {'e': 8},
+            8: {'m': 16, 'n': 22},
+            9: {'a': 10},
+            10: {'j': 11},
+            11: {'a': 12},
+            12: {'r': 100},  # "belajar"
+            16: {'b': 17},
+            17: {'a': 18},
+            18: {'c': 19},
+            19: {'a': 100},  # "membaca"
+            22: {'o': 23},
+            23: {'n': 24},
+            24: {'t': 25},
+            25: {'o': 26},
+            26: {'n': 100},  # "menonton"
+            18: {'e': 19},
+            19: {'l': 20},
+            20: {'i': 100}   # "membeli"
         }
         self.final_state = 100
 
-    def recognize(self, word, fsm_transitions):
+    def recognize(self, word):
         curr_state = 0  # Inisialisasi state awal
         for letter in word:
-            if curr_state in fsm_transitions and letter in fsm_transitions[curr_state]:
-                curr_state = fsm_transitions[curr_state][
-                    letter
-                ]  # Pindah ke state berikutnya
+            if curr_state in self.transitions and letter in self.transitions[curr_state]:
+                curr_state = self.transitions[curr_state][letter]  # Pindah ke state berikutnya
             else:
-                curr_state = (
-                    -1
-                )  # Jika tidak ada transisi yang sesuai, set state ke -1 (state tidak valid)
+                curr_state = -1  # Jika tidak ada transisi yang sesuai, set state ke -1 (state tidak valid)
                 break
 
-        return (
-            curr_state == self.final_state
-        )  # Mengembalikan True jika state terakhir adalah state final (kata dikenali)
+        return curr_state == self.final_state  # Mengembalikan True jika state terakhir adalah state final (kata dikenali)
 
     def is_predikat(self, word):
-        for predikat, fsm_transitions in self.transitions.items():
-            if self.recognize(word, fsm_transitions):
-                return True
-        return False
+        return self.recognize(word)
--- helper/recognize_s.py
+++ helper/recognize_s.py
@@ -2,57 +2,33 @@ class Recognizer_S:
     def __init__(self):
         # Definisi transisi state untuk mesin keadaan terbatas (finite state machine)
         self.transitions = {
-            "saya": {
-                0: {"s": 1},
-                1: {"a": 2},
-                2: {"y": 3},
-                3: {"a": 100},
-            },
-            "kami": {
-                0: {"k": 1},
-                1: {"a": 2},
-                2: {"m": 3},
-                3: {"i": 100},
-            },
-            "anda": {
-                0: {"a": 1},
-                1: {"n": 2},
-                2: {"d": 3},
-                3: {"a": 100},
-            },
-            "kita": {
-                0: {"k": 1},
-                1: {"i": 2},
-                2: {"t": 3},
-                3: {"a": 100},
-            },
-            "dia": {
-                0: {"d": 1},
-                1: {"i": 2},
-                2: {"a": 100},
-            },
+            0: {'s': 1, 'k': 4, 'a': 7, 'd': 10},
+            1: {'a': 2},
+            2: {'y': 3},
+            3: {'a': 100},  # "saya"
+            4: {'a': 5, 'i': 12},
+            5: {'m': 6},
+            6: {'i': 100},  # "kami"
+            7: {'n': 8},
+            8: {'d': 9},
+            9: {'a': 100},  # "anda"
+            10: {'i': 11},
+            11: {'a': 100},  # "dia"
+            12: {'t': 13},
+            13: {'a': 100},  # "kita"
         }
         self.final_state = 100
 
-    def recognize(self, word, fsm_transitions):
+    def recognize(self, word):
         curr_state = 0  # Inisialisasi state awal
         for letter in word:
-            if curr_state in fsm_transitions and letter in fsm_transitions[curr_state]:
-                curr_state = fsm_transitions[curr_state][
-                    letter
-                ]  # Pindah ke state berikutnya
+            if curr_state in self.transitions and letter in self.transitions[curr_state]:
+                curr_state = self.transitions[curr_state][letter]  # Pindah ke state berikutnya
             else:
-                curr_state = (
-                    -1
-                )  # Jika tidak ada transisi yang sesuai, set state ke -1 (state tidak valid)
+                curr_state = -1  # Jika tidak ada transisi yang sesuai, set state ke -1 (state tidak valid)
                 break
 
-        return (
-            curr_state == self.final_state
-        )  # Mengembalikan True jika state terakhir adalah state final (kata dikenali)
+        return curr_state == self.final_state  # Mengembalikan True jika state terakhir adalah state final (kata dikenali)
 
     def is_subject(self, word):
-        for subject, fsm_transitions in self.transitions.items():
-            if self.recognize(word, fsm_transitions):
-                return True
-        return False
+        return self.recognize(word)
